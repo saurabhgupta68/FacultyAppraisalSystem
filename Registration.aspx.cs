7a5ae31f90dd6c6e7a1babea4b20713dedc1e2d3
saurabhgupta68 <saurabh.sg68@gmail.com>
2017-02-17 23:31:55 +0530

    Add files via upload

---
+++ Registration.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Registration : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+            conn.Open();
+            string checkuser = "select count(*) from table where UserName='"+ TextBoxUN .Text+ "'";
+            SqlCommand com = new SqlCommand(checkuser, conn);
+            int tempt = Convert.ToInt32(com.ExecuteScalar().ToString());
+            if(tempt==1)
+            {
+                Response.Write("user already exists");
+
+            }
+            conn.Close();
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Guid newGUID = Guid.NewGuid();
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+            conn.Open();
+            string insertquery = "insert into table(ID,UserName,Email,Password) values  (@ID, @Uname, @email , @password)";
+            SqlCommand com = new SqlCommand(insertquery, conn);
+            com.Parameters.AddWithValue("@ID", newGUID.ToString());
+            com.Parameters.AddWithValue("@Uname", TextBoxUN.Text);
+            com.Parameters.AddWithValue("@email", TextBoxEmail.Text);
+            com.Parameters.AddWithValue("@password", TextBoxPass.Text);
+            com.ExecuteNonQuery();
+            Response.Redirect("Manager.aspx");
+            Response.Write("Resgistration is successful");
+             conn.Close();
+        }
+        catch (Exception ex)
+        {
+            Response.Write("error:" + ex.ToString());
+        }
+    }
+}
